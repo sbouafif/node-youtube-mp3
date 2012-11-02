6d6ecb54779815466d91d7274276acdeadff8509
Slim Bouafif <slim.bouafif@gmail.com>
2012-11-02 10:03:10 +0100

    spawn added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 
-var _ = require('underscore') // for some utility goodness
+var spawn = require('child_process').spawn
+ ,  _ = require('underscore') // for some utility goodness
  , getmp3 = spawn('sh', [ 'getmp3.sh' ], {
   cwd: process.env.HOME + '/Workspace/youtube-mp3/sh',
   env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
